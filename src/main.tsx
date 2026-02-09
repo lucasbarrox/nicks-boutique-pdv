4bc3608471e98c3cd68537c79fd72b1dca1f5c9e
Lucas Barros <42456678+lucasbarrox@users.noreply.github.com>
2026-02-09 07:51:17 +0000

    remover inicialização do banco de dados no arquivo main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import './styles/globals.css';
-import { initializeDb } from './lib/seed.ts';
-
-initializeDb();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
